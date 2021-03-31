4ab102cb0201b1f887b8b5e5df9f7e88ce8eedda
RamyaK <ext_rakulkarni@falabella.cl>
2021-03-31 13:49:18 +0530

    Added a new test for launching Browser using WebDriverManager

--- src/test/java/LaunchBrowser/LaunchingBrowser.java
+++ src/test/java/LaunchBrowser/LaunchingBrowser.java
@@ -1,10 +1,14 @@
 package LaunchBrowser;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class LaunchingBrowser {
-    @Test
+
+    // Traditional method
+    @Test(enabled = false)
     public void testLaunchingBrowser() {
 
         System.setProperty("webdriver.chrome.driver", "utilities/drivers/chromedriver 4");
@@ -13,4 +17,15 @@ public class LaunchingBrowser {
         driver.get("https://ramyakulkarni.wordpress.com/");
 
     }
+
+    // Using WebDriverManager library, no need to specify the location of the browser or download the exe files.
+    @Test
+    public void testLaunchingBrowserWithWebDriverManager() {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://ramyakulkarni.wordpress.com");
+
+    }
 }
+
