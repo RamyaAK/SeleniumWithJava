0712742888345e1c3a3160de01ce27dfe4d51896
RamyaK <ext_rakulkarni@falabella.cl>
2021-05-27 14:13:28 +0530

    Adding autosuggestions automation using java Selenium

---
+++ src/test/java/AutoSuggestions/handleAutoSuggestions.java
@@ -0,0 +1,36 @@
+package AutoSuggestions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import java.util.List;
+
+
+public class handleAutoSuggestions {
+    @Test
+    public void testAutoSuggestions() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.co.in/");
+
+        WebElement searchKeyword = driver.findElement(By.name("q"));
+        searchKeyword.sendKeys("humming");
+        Thread.sleep(4000);
+
+        List<WebElement> options = driver.findElements(By.xpath("//li[@class='sbct']//div[@role='option']//div[1]//span"));
+
+        System.out.println(options.size());
+
+        for(WebElement option : options){
+            if(option.getText().equalsIgnoreCase("humming meaning")){
+                option.click();
+                break;
+            }
+        }
+        driver.quit();
+    }
+}
+
